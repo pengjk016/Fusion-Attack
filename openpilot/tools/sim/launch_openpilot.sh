78a6e91a36bc73d44e22553a887c88e96a0b80e3
pengjk016 <148619603+pengjk016@users.noreply.github.com>
2026-06-03 13:26:15 +0800

    Update launch_openpilot.sh

--- openpilot/tools/sim/launch_openpilot.sh
+++ openpilot/tools/sim/launch_openpilot.sh
@@ -7,7 +7,7 @@ export SIMULATION="1"
 export SKIP_FW_QUERY="1"
 export FINGERPRINT="HONDA CIVIC 2016"
 export MAPBOX_TOKEN="1234"
-export BLOCK="${BLOCK},camerad,loggerd,encoderd,micd,logmessaged"
+export BLOCK="${BLOCK},camerad,loggerd,encoderd,micd,logmessaged,mapsd,navd,navmodeld"
 if [[ "$CI" ]]; then
   # TODO: offscreen UI should work
   export BLOCK="${BLOCK},ui"
